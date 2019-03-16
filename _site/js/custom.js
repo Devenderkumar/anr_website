bf58608d993d2b859c780849e8d9922c91fb322f
David Bruce Borenstein <david@990consulting.com>
2019-03-16 11:35:47 -0400

    Created "blog" branch with blog static mockup.

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -1,44 +1,64 @@
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-	  var target = $(this.hash);
-	  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	  if (target.length) {
-		$('html,body').animate({
-		  scrollTop: target.offset().top - 69
-		}, 1000);
-		return false;
-	  }
-	}
-  });
+// Navigation
+var lastId,
+    topMenu = $(".main-nav"),
+    topMenuHeight = 70,
+    menuItems = topMenu.find("a"),
+    scrollItems = menuItems.map(function() {
+        var item = $($(this).attr("href"));
+        if (item.length) { return item; }
+    });
+
+menuItems.click(function(e) {
+    var href = $(this).attr("href"),
+        offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+    $('html, body').stop().animate({
+        scrollTop: offsetTop
+    }, 1000);
+    e.preventDefault();
 });
 
-$('.mb-nav-btn').click(function () {
-	$('html, body, .main-content-wrap').toggleClass('active')
+
+$(window).scroll(function() {
+    var fromTop = $(this).scrollTop() + topMenuHeight;
+    var cur = scrollItems.map(function() {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+    if (lastId !== id) {
+        lastId = id;
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href='#" + id + "']").parent().addClass("active");
+    }
 });
-	
-$('.main-content-wrap, .mobile-main-nav a').click(function () {
-	$('html, body, .main-content-wrap').removeClass('active')
+
+$('.mb-nav-btn').click(function() {
+    $('html, body, .main-content-wrap').toggleClass('active')
 });
-	
-$('#btn_submit').click(function () {
+
+$('.main-nav a').click(function() {
+    $('html, body, .main-content-wrap').removeClass('active')
+});
+
+$('#btn_submit').click(function() {
     // console.log('ok');
-    
-    $.post("https://notify.appliednonprofitresearch.com/notify",
-    JSON.stringify({
-        name: document.getElementById('name').value,
-        email: document.getElementById('email').value,
-        subject: document.getElementById('subject').value,
-        message: document.getElementById('message').value,
-    }),
-      function(data, status){
-          document.getElementById("contact_form").reset();
 
-          $('#form_submitted').delay(500).fadeIn('normal', function() {
-              $(this).delay(2500).fadeOut();
-          });
+    $.post("https://notify.990consulting.com/990_inquiry"), {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            subject: document.getElementById('subject').value,
+            message: document.getElementById('message').value,
+        },
+        function(data, status) {
+            document.getElementById("contact_form").reset();
 
-      });
+            $('#form_submitted').delay(500).fadeIn('normal', function() {
+                $(this).delay(2500).fadeOut();
+            });
+
+        };
     // https://github.com/anr990/anr_website/blob/5cfe2e2f5d39c83d5b1b8ca5eedbc8e638f9772e/js/custom5010.js
     // $('html, body, .main-content-wrap').toggleClass('active')
 });
